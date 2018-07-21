11061ec5669f21688bb3d75fb36615b712fc71bc
Tyler Lewis <tylerlewisjcg@gmail.com>
2018-07-21 16:19:37 -0600

    assets named

--- src/components/WeddingParty.js
+++ src/components/WeddingParty.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navbar from './Navbar';
-
+import Weston_Clements from './../assets/pictures/Weston_Clements.png'
 const WeddingParty = () => {
     return ( <div>
 <Navbar/>
-        
+        <img src={Weston_Clements} width="200px"/>
     </div> );
 }
  
