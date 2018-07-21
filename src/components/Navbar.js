4b0f930cf6c60a95707ca86defb3595cb8317a50
Tyler Lewis <tylerlewisjcg@gmail.com>
2018-07-21 13:45:19 -0600

    wedding colors added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
+import './../styles/navbar.css'
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      NAVBAR COMPONENT
-      <ul>
+    <div className="navbar">
+      <ul className="nav_links">
         <Link to="/">
           <li>Home</li>
         </Link>
